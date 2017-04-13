5441dd3c4d7badf6c21128d0aba79ea8544f8671
IQ Plans <ijaz.haq@hotmail.com>
2017-04-13 12:28:31 +0500

    MyFirstCommit

---
+++ ZariApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ZariApp.Startup))]
+namespace ZariApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
